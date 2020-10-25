ecc104565eefcd2e40551e1c6e0b57b007d1cef6
ada <adajames81@gmail.com>
2020-10-25 18:40:35 -0500

    first commit

---
+++ learning_templates/basic_app/urls.py
@@ -0,0 +1,17 @@
+# from django.conf.urls import url
+from basic_app import views
+from django.urls import path
+
+# for template tagging django looks for app_name and "other" in the path
+app_name = 'basic_app'
+
+urlpatterns = [
+
+    path('relative/', views.relative,name='relative'),
+    path('other/', views.other,name='other'),
+
+
+
+
+
+]
